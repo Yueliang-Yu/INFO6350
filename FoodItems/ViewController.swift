86e16148efd326c4bc5adbb287c2df388ce5b831
yuyueliang <2469536487@qq.com>
2023-10-01 23:53:31 -0700

    Initial Commit

---
+++ FoodItems/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoodItems
+//
+//  Created by 余玥良 on 2023/10/1.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
